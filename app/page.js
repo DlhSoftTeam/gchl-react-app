a0a6d33a8a2db9d820beb7e8bcaef4991842011a
Sorin Dolha <sdolha@Mac-Sorin.local>
2024-09-22 10:55:09 +0300

    Components

--- app/page.js
+++ app/page.js
@@ -1,93 +1,67 @@
+"use client"
+
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { GanttChartView, ScheduleChartView, LoadChartView } from "../public/DlhSoft.ProjectData.GanttChart.React.Components";
+import { PertChartView, NetworkDiagramView } from "../public/DlhSoft.ProjectData.PertChart.React.Components";
+
+// Set up your DlhSoft Gantt Chart Hyper Library product license here:
+const gchlLicense = "...";
 
 export default function Home() {
+  // Define items, settings, and change handler for GanttChartView.
+  // Note: use setItems if you need to update GanttChartView's data later.
+  const [items, setItems] = useState([{ content: 'Item 1' }]);
+  const settings = { 
+    weekStartDay: 1,
+  };
+  function onItemChanged(item, propertyName, isDirect, isFinal) {
+    console.log("Item " + item.content + " has changed (property " + propertyName + " has become " + item[propertyName] + ").");
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1>DlhSoft Gantt Chart Hyper Library – React/next.js demo</h1>
 
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+
+          {/* Use GanttChartView component. */}
+          <GanttChartView items={items} settings={settings} license={gchlLicense}
+                          change={onItemChanged} style={{width: "960px", height: "350px"}}/>
+
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      <a
+          href="https://dlhsoft.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
             src="https://nextjs.org/icons/globe.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          DlhSoft
+        </a>
+
+        <a
+          href="https://dlhsoft.com/GCHL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
             alt="Globe icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Ganttt Chart Hyper Library →
         </a>
       </footer>
     </div>
